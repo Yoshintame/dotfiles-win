87c1ffe36ed1f9330ad5635ca88ba2309b0ec35e
Mikhail Ivanov <m.ivanov0427@gmail.com>
2022-09-02 00:12:13 +0300

    true-dark spicetify theme added

--- spicetify/install.ps1
+++ spicetify/install.ps1
@@ -3,6 +3,7 @@ gsudo choco install -y spotify spicetify-cli spicetify-marketplace
 
 $SpicetifyThemeLocal = "$($env:AppData)\spicetify"
 git clone https://github.com/Yoshintame/spicetify-fluent-theme.git "$($SpicetifyThemeLocal)\Themes\Fluent"
+git clone https://github.com/Yoshintame/spicetify-true-dark-theme.git "$($SpicetifyThemeLocal)\Themes\TrueDark"
 # Copy-Item -Path "$($PSScriptRoot)\themes\fluent" "$($SpicetifyThemeLocal)\Themes\Fluent" -Force -Recurse
 
 spicetify config current_theme fluent
